5d5aa9abb77a0671bbdef304571860cdb7ff1938
nathangaert <ngaert06@gmail.com>
2025-03-08 11:43:56 -0500

    Got flask first page working

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-data-gathering
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def menu():
     return render_template('menu.html')
 
+def submit_interests():
+    data = request.get_json()
+    
+    interests = data.get("interests")
+    timestamp = data.get("timestamp")
+    
+    # Log received data for debugging
+    print("Received interests:", interests)
+    print("Timestamp:", timestamp)
+    
+    #Process data
+    #For now, just log it
+    
+    return jsonify({"success": True, "message": "Interests received!"})
+    
 if __name__ == '__main__':
     app.run(port=54321, debug=True)
